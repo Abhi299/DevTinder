ee76ecdc733a713457dc95c84def9ceb478950aa
Abhi299 <abhimishra299@gmail.com>
2026-04-04 16:12:19 +0530

    Update database connection to use fallback MongoDB URL if environment variable is not set

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,10 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  await mongoose.connect(process.env.MONGODB_URL);
+  await mongoose.connect(
+    process.env.MONGODB_URL ||
+      "mongodb+srv://abhimishra299_db_user:2gYjr2H83isX6HVC@devtinder.wndmhcn.mongodb.net/?appName=DevTinder",
+  );
   console.log("database connected successfully!");
 };
 
